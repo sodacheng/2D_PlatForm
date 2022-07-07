188608099d62ec9b8778bcbfd28bf599e1b74d4e
橙飒 <634377685@qq.com>
2022-07-07 15:52:32 +0800

    2D平台动作项目导入
    Part5

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,255 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float movementInputDirection; // 水平输入
+
+    private int amountOfJumpsLeft; // 剩余跳跃次数计数
+    private int facingDirection = 1;
+
+    private bool isFacingRight = true; // 是否面向右方
+    private bool isWalking; // 是否在走路
+    private bool isGrounded; // 是否接触地面
+    private bool isTouchingWall; // 是否接触墙面
+    private bool isWallSliding; // 是否在滑墙
+    private bool canJump; // 是否能跳跃
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    public int amountOfJumps = 1; // 主角能跳跃的次数
+
+    public float movementSpeed = 10.0f; // 移动速度
+    public float jumpForce = 16.0f; // 跳跃力(设置速度)
+    public float groundCheckRadius; // 地面检测半径范围
+    public float wallCheckDistance; // 墙壁检测距离
+    public float wallSlideSpeed; // 滑墙最大速度
+    public float movementForceInAir; 
+    public float airDragMultiplier = 0.95f;
+    public float variableJumpHeightMultiplier = 0.5f;
+    public float wallHopForce;
+    public float wallJumpForce;
+
+    public Vector2 wallHopDirection;
+    public Vector2 wallJumpDirection;
+
+    public Transform groundCheck;
+    public Transform wallCheck;
+
+    public LayerMask whatIsGround; // Layer层 -> 在Inspector中选择Ground层
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
+        wallHopDirection.Normalize();
+        wallJumpDirection.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
+        CheckIfWallSliding(); // 检测当前是否该进行滑墙
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+        CheckSurroundings();
+    }
+
+    /// <summary>
+    /// 检测当前是否该进行滑墙
+    /// </summary>
+    private void CheckIfWallSliding()
+    {
+        if (isTouchingWall && !isGrounded && rb.velocity.y < 0)
+        {
+            isWallSliding = true;
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    /// <summary>
+    /// 判断Player周边状态, 是否触地, 是否触墙等
+    /// </summary>
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+    }
+
+    /// <summary>
+    /// 判断Player能否进行跳跃
+    /// </summary>
+    private void CheckIfCanJump()
+    {
+        if((isGrounded && rb.velocity.y <= 0) || isWallSliding)
+        {
+            amountOfJumpsLeft = amountOfJumps; // 当 Player 接触地面 或者 滑墙时 重置 Player跳跃次数
+        }
+
+        if(amountOfJumpsLeft <= 0)
+        {
+            canJump = false; // 跳跃计数用完则不能跳跃
+        }
+        else
+        {
+            canJump = true;
+        }
+      
+    }
+
+    /// <summary>
+    /// 移动方向相关判断
+    /// 根据移动方向控制角色专项
+    /// 判断角色是否在水平方向上走动
+    /// </summary>
+    private void CheckMovementDirection()
+    {
+        if(isFacingRight && movementInputDirection < 0)
+        {
+            Flip();
+        }
+        else if(!isFacingRight && movementInputDirection > 0)
+        {
+            Flip();
+        }
+
+        if(rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+    /// <summary>
+    /// 更新动画状态机相关变量
+    /// </summary>
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isWallSliding", isWallSliding);
+    }
+
+    /// <summary>
+    /// 输入检测
+    /// </summary>
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
+        }
+
+    }
+
+    /// <summary>
+    /// 跳跃
+    /// </summary>
+    private void Jump()
+    {
+        if (canJump && !isWallSliding)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
+        else if (isWallSliding && movementInputDirection == 0 && canJump) //Wall hop
+        {
+            isWallSliding = false;
+            amountOfJumpsLeft--;
+            Vector2 forceToAdd = new Vector2(wallHopForce * wallHopDirection.x * -facingDirection, wallHopForce * wallHopDirection.y);
+            rb.AddForce(forceToAdd, ForceMode2D.Impulse);
+        }
+        else if((isWallSliding || isTouchingWall) && movementInputDirection != 0 && canJump)
+        {
+            isWallSliding = false;
+            amountOfJumpsLeft--;
+            Vector2 forceToAdd = new Vector2(wallJumpForce * wallJumpDirection.x * movementInputDirection, wallJumpForce * wallJumpDirection.y);
+            rb.AddForce(forceToAdd, ForceMode2D.Impulse);
+        }
+    }
+
+    /// <summary>
+    /// 驱动角色移动
+    /// </summary>
+    private void ApplyMovement()
+    {
+        // 当角色在地面
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+        }
+        else if(!isGrounded && !isWallSliding && movementInputDirection != 0) // 角色在空中且有水平输入
+        {
+            Vector2 forceToAdd = new Vector2(movementForceInAir * movementInputDirection, 0);
+            rb.AddForce(forceToAdd);
+
+            if(Mathf.Abs(rb.velocity.x) > movementSpeed)
+            {
+                rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+            }
+        }
+        else if(!isGrounded && !isWallSliding && movementInputDirection == 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x * airDragMultiplier, rb.velocity.y);
+        }
+
+        if (isWallSliding)
+        {
+            if(rb.velocity.y < -wallSlideSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -wallSlideSpeed);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 翻转角色
+    /// </summary>
+    private void Flip()
+    {
+        // 当角色非滑墙状态时才会翻转
+        if (!isWallSliding)
+        {
+            facingDirection *= -1;
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0.0f, 180.0f, 0.0f);
+        }
+    }
+
+    /// <summary>
+    /// 在Scene标注判定范围变化
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
+    }
+}
